1a21a646cba067ecfe86713146f38f82313dd5e7
Avishka <eaahashara01@gmail.com>
2026-03-15 15:23:01 +0530

    Add new menu route to web.php for PageController

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\PageController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/menu', [PageController::class, 'menu'])->name('menu'); // New Menu Route
